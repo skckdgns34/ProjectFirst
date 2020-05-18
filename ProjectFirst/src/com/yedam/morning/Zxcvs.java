2e33bf0086e63e50b28cc3e551ab774c5fa58705
choich <skckdgns34@naver.com>
2020-05-18 12:51:44 +0900

    5/18

---
+++ ProjectFirst/src/com/yedam/morning/Zxcvs.java
@@ -0,0 +1,19 @@
+package com.yedam.morning;
+
+//일반 클래스
+public class Zxcvs {
+	
+	private int a; //필드 들
+	private int b;
+	private int c;
+	
+	public void Zxcvs(int a, int b, int c) { //생성자
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	
+	public void x() { //메소드
+		int x; //변수
+	}
+}
